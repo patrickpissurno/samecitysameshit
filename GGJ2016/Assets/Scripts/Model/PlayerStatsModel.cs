3cebb296a421e66e61fd73ba0cee3f4d43353151
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-01-30 21:48:23 -0200

    Boss Hapiness UI Implemented

---
+++ GGJ2016/Assets/Scripts/Model/PlayerStatsModel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatsModel {
+    private float hapiness = 1f;
+    public float Hapiness
+    {
+        get
+        {
+            return hapiness;
+        }
+        set
+        {
+            if (value > 1)
+                value = 1;
+            else if (value < 0)
+                value = 0;
+            hapiness = value;
+        }
+    }
+}
--- GGJ2016/Assets/Scripts/View/GameUIView.cs
+++ GGJ2016/Assets/Scripts/View/GameUIView.cs
@@ -10,9 +10,12 @@ public class GameUIView : MonoBehaviour {
     public Image BossIndicator;
     public PauseMenuView PauseMenuView;
     private IGameUIService Service;
+    private static Sprite[] BossSprites;
+    private int BossSpriteIndex = 0;
 
 	void Start () {
         Service = new GameUIService();
+        LoadBossSprites();
         UpdateMonthView();
 	}
 
@@ -21,6 +24,7 @@ public class GameUIView : MonoBehaviour {
         Service.UpdateTimer(Time.deltaTime);
         UpdateClockView();
         UpdateDayView();
+        UpdateBossSprite();
     }
 
     public void PauseButtonClick()
@@ -43,8 +47,26 @@ public class GameUIView : MonoBehaviour {
         Month.text = Service.GetRandomMonth();
     }
 
+    void UpdateBossSprite()
+    {
+        int spriteIndex = Mathf.FloorToInt(8 - 8 * Service.GetHappiness());
+        if (spriteIndex > 7)
+            spriteIndex = 7;
+        if (BossSpriteIndex != spriteIndex)
+        {
+            BossSpriteIndex = spriteIndex;
+            BossIndicator.sprite = BossSprites[BossSpriteIndex];
+        }
+    }
+
     public IGameUIService GetService()
     {
         return Service;
+    }
+
+    void LoadBossSprites()
+    {
+        if(BossSprites == null)
+            BossSprites = Resources.LoadAll<Sprite>("Sprites/boss");
     }
 }
