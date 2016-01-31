4dc2b9cb45489c9da5b2357bc278290ee0449e04
PabloAlexandre <pabloalexandre.nave@gmail.com>
2016-01-31 14:36:26 -0200

    Audio

--- GGJ2016/Assets/Scripts/View/MainMenuView.cs
+++ GGJ2016/Assets/Scripts/View/MainMenuView.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class MainMenuView : MonoBehaviour {
     private IMainMenuService MainMenuService;
+    public Transform mainAudio;
+
 	void Start () {
         MainMenuService = new MainMenuService();
+
+        if (!GameObject.Find("Audio_Prefab_Component")) {
+            Transform t = Instantiate(mainAudio, mainAudio.position, mainAudio.rotation) as Transform;
+            t.name = "Audio_Prefab_Component";
+            DontDestroyOnLoad(t.gameObject);
+        }
 	}
 
     public void PlayClicked()
