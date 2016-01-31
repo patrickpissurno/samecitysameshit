2de3f3722b0ef746c3d31ae356e1a4bf9bc18a4f
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-01-31 04:10:25 -0200

    Vehicle Movement System Implemented

---
+++ GGJ2016/Assets/Scripts/View/TransportEntityView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TransportEntityView : MonoBehaviour {
+    [HideInInspector]
+    public SpawnerView Spawner;
+    [HideInInspector]
+    public SpawnerView.EntityType Type;
+    public float Speed = 3;
+	public void SafeDestroy()
+    {
+        if (Spawner != null)
+            Spawner.Recicle(Type, gameObject);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag.Equals("EntityWall"))
+        {
+            SafeDestroy();
+        }
+    }
+}
