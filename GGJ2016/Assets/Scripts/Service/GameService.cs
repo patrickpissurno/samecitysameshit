5aa9c1056e3b987651e97d3c3f5f836bb635ab1c
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-01-31 10:10:16 -0200

    Merge

--- GGJ2016/Assets/Scripts/Service/GameService.cs
+++ GGJ2016/Assets/Scripts/Service/GameService.cs
@@ -15,6 +15,7 @@ public class GameService : IGameService
     private GameObject currentGameObject;
 
     private GameObject camObject;
+    private Animation anim;
 
     public void SetupGameView(GameView gameView)
     {
@@ -23,6 +24,11 @@ public class GameService : IGameService
         InputManager.instance.onClickListener += OnClick;
 
         camObject = GameObject.Find(ElementType.MainCamera.ToString());
+
+        if (anim == null)
+        {
+            anim = playerObject.GetComponent<Animation>();
+        }
     }
 
     public void MovePlayer()
@@ -32,6 +38,29 @@ public class GameService : IGameService
             if (currentGameObject.name.Equals(TagType.Limit))
             {
                 LoadPlayer();
+                Vector3 forward = player.getTargetPosition() - playerObject.transform.position;
+                forward.y = 0;
+
+                if (Vector3.Distance(Vector3.zero, forward) > 0.2f && player.getTargetPosition() != Vector3.zero)
+                {
+                    Quaternion direction = Quaternion.LookRotation(forward);
+                    playerObject.transform.rotation = Quaternion.Slerp(playerObject.transform.rotation, direction, speed * 4 * Time.deltaTime);
+                }
+
+                if (Vector3.Distance(Vector3.zero, forward) > 0.1f)
+                {
+                    if (!anim.IsPlaying("Walk"))
+                    {
+                        anim.CrossFade("Walk", 0.3f);
+                    }
+                }
+                else {
+                    if (!anim.IsPlaying("Idle"))
+                    {
+                        anim.CrossFade("Idle", 0.3f);
+                    }
+                }
+
                 playerObject.transform.position = Vector3.MoveTowards(playerObject.transform.position, player.getTargetPosition(), speed * Time.deltaTime);
                 SetFixedPosition();
             }
@@ -47,7 +76,7 @@ public class GameService : IGameService
     private void SetFixedPosition()
     {
         LoadPlayer();
-        playerObject.transform.position = new Vector3(playerObject.transform.position.x, 0.7f, playerObject.transform.position.z);
+        playerObject.transform.position = new Vector3(playerObject.transform.position.x, 0.16f, playerObject.transform.position.z);
     }
 
     void OnClick(GameObject gameObject, Vector3 clickPosition)
@@ -73,11 +102,25 @@ public class GameService : IGameService
                 Debug.Log(gameObject.tag);
                 break;
 
+            case TagType.Bus:
+
+                break;
+
+            case TagType.Taxi:
+
+                break;
+
+            case TagType.Rebu:
+
+                break;
+
+            case TagType.Garage:
+                RunAnimOpenGarage(gameObject);
+                break;
+
             default:
                 break;
         }
-
-
         player.SetCurrentPosition(playerObject.transform.position);
     }
 
@@ -100,4 +143,14 @@ public class GameService : IGameService
     {
         camObject.GetComponent<Animator>().SetBool("canZoom", false);
     }
+
+    public void RunAnimOpenGarage(GameObject gameobject)
+    {
+        gameobject.GetComponent<Animator>().SetBool("canOpen", true);
+    }
+
+    //public void RunAnimCloseGarage()
+    //{
+    //    GameObject.Find(TagType.Garage.ToString()).GetComponent<Animator>().SetBool("canOpen", false);
+    //}
 }
