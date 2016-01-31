98ced92bec2e933eeff9f9e0e65e2e51f70b104d
Bruno Vieira <brunovieira.nave@gmail.com>
2016-01-31 06:16:42 -0200

    add new tags

--- GGJ2016/Assets/Scripts/Domain/TagType.cs
+++ GGJ2016/Assets/Scripts/Domain/TagType.cs
@@ -4,4 +4,10 @@ using System.Collections;
 public class TagType {
     public const string BusStop = "BusStop";
     public const string Limit = "Limit";
+    public const string Bus = "Bus";
+    public const string Car = "Car";
+    public const string Taxi = "Taxi";
+    public const string Rebu = "Rebu";
+    public const string Train = "Train";
+    public const string Garage = "Garage";
 }
