63a7f6cc34d3bbbc597c0a9d2cc17244b591eaf3
Pablo Alexandre <pabloalexandre.nave@gmail.com>
2016-01-31 08:08:35 -0200

    Cutscene Epic System

---
+++ GGJ2016/Assets/Scripts/Model/MovePathModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class MovePathModel {
+
+    public float waitForBeggining;
+    public float speed;
+    public float waitForStop;
+
+    public bool useRotation;
+
+    public string playAnimation;
+    public string changeSceneOnEnd;
+
+    public GameObject target;
+    public string callMethodOnEnd;
+
+    public Transform[] paths;
+}
