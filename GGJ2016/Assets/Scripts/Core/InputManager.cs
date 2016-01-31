7020657ea632fcbd42f6be481a0cb0f695f3716c
Bruno Vieira <brunovieira.nave@gmail.com>
2016-01-30 23:07:11 -0200

    InputManager refactored

--- GGJ2016/Assets/Scripts/Core/InputManager.cs
+++ GGJ2016/Assets/Scripts/Core/InputManager.cs
@@ -2,36 +2,77 @@
 using System;
 using System.Collections;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
 
     public static Action<GameObject, Vector3> onClickListener;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(Input.GetMouseButtonDown(0)){
-            #if UNITY_ANDROID
-                Vector3 position = Input.GetTouch(0).position;
-            #else
-                Vector3 position = Input.mousePosition;
-            #endif
+    public static Action<Vector3> onCameraClickPressedListener;
 
-            Ray ray = Camera.main.ScreenPointToRay(position);
-            OnMouseDown(this.gameObject, ray);
+    public static Action<Vector3> onCameraClickUpListener;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(setupPlataformPosition());
+            OnClickDown(this.gameObject, ray);
         }
 
-	}
+        else if (Input.GetMouseButton(0))
+        {
+            OnCameraClickPressed();
+        }
 
-    private void OnMouseDown(GameObject gameObject, Ray ray)
+        else if (Input.GetMouseButtonUp(0))
+        {
+            OnCameraClickUp();
+        }
+    }
+
+
+    private Vector3 setupPlataformPosition()
+    {
+        #if UNITY_ANDROID
+            Vector3 position = Input.GetTouch(0).position;
+        #else
+            Vector3 position = Input.mousePosition;
+        #endif
+
+        return position;
+    }
+
+    private void OnClickDown(GameObject gameObject, Ray ray)
     {
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 1000))
-        {
             onClickListener(hit.transform.gameObject, hit.point);
-        }
+    }
+
+    private void OnCameraClickPressed()
+    {
+        onCameraClickPressedListener(setupPlaneVector());
+    }
+
+    private void OnCameraClickUp()
+    {
+        onCameraClickUpListener(setupPlaneVector());
+    }
+
+    private Vector3 setupPlaneVector()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, 0));
+        float distance;
+        xy.Raycast(ray, out distance);
+
+        return ray.GetPoint(distance);
     }
 }
