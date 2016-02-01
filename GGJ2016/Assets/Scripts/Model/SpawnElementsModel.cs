6582101b6b3ec22521469793914e96a816674a14
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-02-01 16:48:12 -0200

    [Refactoring] Removed Unused/Outdated assets
    - Reorganizated models and project folder structure
    - Removed duplicated textures and materials

--- GGJ2016/Assets/Scripts/Model/SpawnElementsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[System.Serializable]
-public class SpawnElementsModel  {
-    public StreetSide side;
-    public SpawnType element;
-    public float speed;
-}
